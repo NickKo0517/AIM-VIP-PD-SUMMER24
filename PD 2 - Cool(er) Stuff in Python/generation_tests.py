figured out bug in vertical gradient pattern

PD 2 - Cool(er) Stuff in Python/generation_tests.py
@@ -47,7 +47,7 @@ class generation_tests(unittest.TestCase):
         for i in range(im_size):
             for j in range(im_size):
                 self.assertEqual(image[i][j], image[j][i], msg=f"image[{i}][{j}] != image[{j}][{i}]")
-        # save image as grayscale to local dir for view
+        # save image as grayscale to local directory for view
         # plt.imsave("diagonal.png", image, cmap="gray")
     
     """     gradient tests     """
@@ -59,11 +59,10 @@ class generation_tests(unittest.TestCase):
                          got {gradGen_obj.name}")
         # generate_image tests
         seed = randbits(128)
+        direction = seed % 8
         image = gradGen_obj.generate_image(seed=seed)
         self.assertEqual(image.shape, (gradGen_obj.image_size, gradGen_obj.image_size),
-                         msg=f"expected (50,50), got {image.shape} when seed = {seed}")
-
-        direction = seed % 8
+                         msg=f"expected (50,50), got {image.shape} when seed = {seed}, direction = {direction}")
         print("direction = {}".format(direction))
         if direction == 0:
             # left -> right
@@ -71,18 +70,21 @@ class generation_tests(unittest.TestCase):
             zeroes = np.zeros((image.shape[0], 1))
             self.assertTrue(np.all(image[:,0], axis=0),  # AND all elements in column 0
                             msg=f"expect the first column to be all 0's when seed = {seed}, \
-                            direction = {dir}")
+                            direction = {direction}")
             for col in range(1, image.shape[1]):
                 prev = col - 1
                 # first check if all columns have same value in each entry
                 prev_value, col_value = image[0, prev], image[0, col]
                 self.assertEqual(image[:, prev] / prev_value, np.ones((image.shape[0], 1)),
-                                 msg=f"assertion failed when seed = {seed}, direction  {dir}, at column {col}")
+                                 msg=f"assertion failed when seed = {seed}, direction = {direction}\
+                                 , at column {col}")
                 self.assertEqual(image[:, col] / col_value, np.ones((image.shape[0], 1)),
-                                 msg=f"assertion failed when seed = {seed}, direction  {dir}, at column {col}")
+                                 msg=f"assertion failed when seed = {seed}, direction  {direction},\
+                                 at column {col}")
                 # then check if the prev entries are smaller than col entries by 1
                 self.assertEqual(image[:, col] - image[:, prev], np.ones((image.shape[0], 1)),
-                                 msg=f"assertion failed when seed = {seed}, direction  {dir}, at column {col}")
+                                 msg=f"assertion failed when seed = {seed}, direction  {direction},\
+                                 at column {col}")
         elif direction == 1:
             # right -> left
             # from right to left, first test if the colors are fading
@@ -96,7 +98,7 @@ class generation_tests(unittest.TestCase):
                 self.assertTrue(np.all((image[:, prev] / prev_value), axis=0), msg=f"not all elements are identical in column {prev}")
                 self.assertTrue(np.all((image[:, col] / col_value), axis=0), msg=f"not all elements are identical in column {col}")
                 # then check if the col entries are smaller than prev entries by 1
-                self.asserTrue(np.all((image[:, prev] - image[:, col]), axis=0))
+                self.assertTrue(np.all((image[:, prev] - image[:, col]), axis=0))
         elif direction == 2:
             # top -> bottom
             pass

PD 2 - Cool(er) Stuff in Python/image_generator.py
@@ -81,11 +81,11 @@ class Gradient_Generator(Image_Generator):
             img = np.tile(grad_pattern, (self.image_size, 1))
         elif grad_direction == 2:
             # top to bottom
-            img = np.tile(grad_direction, (1, self.image_size))
+            img = (np.tile(grad_pattern, (self.image_size, 1))).T
         elif grad_direction == 3:
             # bottom to top
             grad_pattern = np.linspace(self.image_size, 0, self.image_size)
-            img = np.tile(grad_direction, (1, self.image_size))
+            img = (np.tile(grad_direction, (1, self.image_size))).T
         elif grad_direction == 4:
             # left upper -> right lower
             pass

PD 2 - Cool(er) Stuff in Python/run_tests.py
@@ -2,8 +2,7 @@ from generation_tests import *
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    unittest.main()
-
+    # unittest.main()
     """     view image generated    """
     # size, name = 50, "testing"
     # image = Diagonal_Pattern_Generator(image_size=size, name=name)
@@ -15,3 +14,10 @@ if __name__ == "__main__":
     
     # """ Save imgArray to another file for debugging and better view """
     # np.savetxt("imgMatrix.txt", imgArray ,fmt="%.2f", delimiter=', ')
+
+    # R = np.linspace(0, 10, 10)
+    R = range(10)
+    img = (np.tile(R, (10, 1))).T
+    print(type(img))
+    print(img.shape)
+    print(img)